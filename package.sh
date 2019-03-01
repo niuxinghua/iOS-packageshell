00af6c029105a7de383e94dc567bf942355f6c00
niuxinghua <niuxinghua@haier.com>
2019-03-01 10:29:07 +0800

    no message
    Change-Id: Ia8674c312f6c007b5237efa4f0315238a97a0b9d

--- package.sh
+++ package.sh
@@ -1,10 +1,10 @@
 # 获取描述文件的uuid并将描述文件copy到系统，前提将系统解锁
-security unlock-keychain -p 5324nxh0506222     /Users/niuxinghua/Library/Keychains/login.keychain
+security unlock-keychain -p mymacpassword     /Users/niuxinghua/Library/Keychains/login.keychain
 
 security import ${P12_Path}.p12 -k /Users/niuxinghua/Library/Keychains/login.keychain -P p12的密码 -T /usr/bin/codesign
 
-mobileprovisionName="COSMOIM"
-mobileprovision_file="/Users/niuxinghua/Desktop/ios/证书/cosmoim/${mobileprovisionName}.mobileprovision"
+mobileprovisionName="kakaxi"
+mobileprovision_file="/Users/niuxinghua/Desktop/ios/证书/kakaxi/${mobileprovisionName}.mobileprovision"
 
 # 将描述文件转换成plist
 mobileprovision_plist="/Users/niuxinghua/Desktop/whocall/${mobileprovisionName}.plist"
